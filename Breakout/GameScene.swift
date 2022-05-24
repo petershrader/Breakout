d19045b8907ab94a1100b67fa0adf33626385630
Peter Shrader <pshrader@bsd220.org>
2022-05-24 10:30:42 -0500

    "Created background"

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -14,20 +14,18 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        let stars = SKTexture(imageNamed: "Stars")
+        for i in 0...1 {
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            addChild(starsBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackground.run(moveForever)
+        }
     }
-    func didMove {
-           let stars = SKTexture(imageNamed: "Stars")
-           for i in 0...1 {
-               let starsBackground = SKSpriteNode(texture: stars)
-               starsBackground.zPosition = -1
-               starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
-               addChild(starsBackground)
-               let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
-               let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
-               let moveLoop = SKAction.sequence([moveDown, moveReset])
-               let moveForever = SKAction.repeatForever(moveLoop)
-               starsBackground.run(moveForever)
-           }
-       }
 }
 
